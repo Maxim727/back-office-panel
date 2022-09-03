3044a22e65b0708e570748c38ed7818f2f06857e
Max <maximlee17@gmail.com>
2022-09-03 08:38:23 +0600

    set up prettier format

--- src/app/shared/modules/tagList/components/tagList/tahgList.component.ts
+++ src/app/shared/modules/tagList/components/tagList/tahgList.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core'
+import { Component, Input } from '@angular/core'
 import { PopularTagType } from 'src/app/shared/types/PopularTagType.type'
 
 @Component({
@@ -7,4 +7,7 @@ import { PopularTagType } from 'src/app/shared/types/PopularTagType.type'
 })
 export class TagsListComponent {
   @Input('tags') tagsProps: PopularTagType[]
+  //@Input('tags') tagsProps: string[]
+  //tagsProps for the access from within component
+  //'tags' interface for the component where this will be injected
 }
